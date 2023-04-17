a727e80b6209b068ded43627e5948f49631282ca
Alessandra Belézia <alebelezia@gmail.com>
2023-04-17 11:36:14 -0300

    Imutabilidade

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,20 @@ import {Card} from '../../components/Card'
 export function Home() {
   const [count, setCount] = useState(0)
   const [studentName, setStudentName] = useState("") 
-  //const [onde guardamos o conteúdo do estado, função que atualiza o estado] 
+  //const [onde guardamos o conteúdo do estado, função que atualiza o estado]
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-BR", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      }),
+    }
+    setStudents((prevState) => [...prevState, newStudent]);
+  }
 
   return (
     <div className='container'>
@@ -16,11 +29,13 @@ export function Home() {
         placeholder="Digite o nome..."
         onChange={(e) => setStudentName(e.target.value)}
       />
-      <button>OK</button>
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
 
-      <Card name="Alessandra" time="10:55:25" />
-      <Card name="João" time="11:00:10" />
-      <Card name="Ana" time="12:10:33" />
+      {students.map((student) => (
+        <Card name={student.name} time={student.time} />
+      ))}
     </div>
   )
 }
