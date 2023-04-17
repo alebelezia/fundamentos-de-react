fcae4d96243bd5f8dd1245fc267ab67e71178f15
Alessandra Belézia <alebelezia@gmail.com>
2023-04-17 11:45:15 -0300

    Definição de Hook

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,5 @@
+//Hooks: São funções que permitem conectar os recursos de estados e ciclos de vida do React a partir de componentes funcionais. Normalmente os Hooks iniciam com a palavra use por convenção. Exemplos de hooks: useState, useEffect.
+
 import { useState } from 'react'
 import './styles.css'
 
