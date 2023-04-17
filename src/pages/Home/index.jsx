e6291583853fc04f72074913d0143cf083ed3208
Alessandra Belézia <alebelezia@gmail.com>
2023-04-17 11:22:04 -0300

    Estado

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,11 +5,17 @@ import {Card} from '../../components/Card'
 
 export function Home() {
   const [count, setCount] = useState(0)
+  const [studentName, setStudentName] = useState("") 
+  //const [onde guardamos o conteúdo do estado, função que atualiza o estado] 
 
   return (
     <div className='container'>
-      <h1>Lista de Presença</h1>
-      <input type="text" />
+      <h1>Nome: {studentName}</h1>
+      <input
+        type="text"
+        placeholder="Digite o nome..."
+        onChange={(e) => setStudentName(e.target.value)}
+      />
       <button>OK</button>
 
       <Card name="Alessandra" time="10:55:25" />
